ddd1b2a485aadc2686476cc0de6b6416f0f4f709
tomo_s <f15c017d@gmail.com>
2021-01-10 12:01:53 +0900

    社員一覧を表示する

---
+++ src/app/mock-members.ts
@@ -0,0 +1,15 @@
+import {Member} from "./member";
+
+
+export const MEMBERS: Member[] = [
+  {id: 11, name: 'aaa'},
+  {id: 12, name: 'bbb'},
+  {id: 13, name: 'ccc'},
+  {id: 14, name: 'ddd'},
+  {id: 15, name: 'eee'},
+  {id: 16, name: 'fff'},
+  {id: 17, name: 'ggg'},
+  {id: 18, name: 'hhh'},
+  {id: 19, name: 'iii'},
+  {id: 20, name: 'jjj'}
+]
