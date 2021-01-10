d6175f911ef4eed990be19b2cf78cf18abba6435
tomo_s <f15c017d@gmail.com>
2021-01-10 12:32:38 +0900

    社員詳細の表示をmember-detailに移植

--- src/app/member-detail/member-detail.component.ts
+++ src/app/member-detail/member-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Member} from "../member";
 
 @Component({
   selector: 'app-member-detail',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-detail.component.css']
 })
 export class MemberDetailComponent implements OnInit {
+  @Input() member: Member;
 
   constructor() { }
 
