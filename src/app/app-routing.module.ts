1e4b73cfecf9db6fe86159604d36321e7ecad722
tomo_s <f15c017d@gmail.com>
2021-01-11 11:21:32 +0900

    自社社員名簿のurlを /members に移動

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes} from '@angular/router';
+import {MembersComponent} from "./members/members.component";
 
+const routes: Routes = [
+  {path: 'members', component: MembersComponent}
+]
 
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class AppRoutingModule { }
