6c89ca4ea60bea4b0ab40b19ce3df2d8dbf3feef
tomo_s <f15c017d@gmail.com>
2021-01-11 17:26:42 +0900

    社員一覧データをhttpクライアントを用いて取得するように

--- src/app/member.service.ts
+++ src/app/member.service.ts
@@ -3,21 +3,30 @@ import {Member} from "./member";
 import {MEMBERS} from "./mock-members";
 import {Observable, of} from "rxjs";
 import {MessageService} from "./message.service";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MemberService {
+  private membersUrl = 'api/members';
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient
+  ) { }
 
   getMembers(): Observable<Member[]> {
     this.messageService.add('MemberService: 社員一覧データを取得しました')
-    return of(MEMBERS)
+    return this.http.get<Member[]>(this.membersUrl)
   }
 
   getMember(id: number): Observable<Member> {
     this.messageService.add(`MemberService: 社員一覧データ(id=${id})を取得しました`)
     return of(MEMBERS.find((member)=>member.id === id))
+  }
+
+  private log(message: string){
+    this.messageService.add(`MemberService: ${message}`)
   }
 }
