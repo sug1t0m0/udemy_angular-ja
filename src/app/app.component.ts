aedac899bf191e5abed219bc0112675dfadab540
tomo_s <f15c017d@gmail.com>
2021-01-15 19:17:04 +0900

    コメントクラスを作成してモックデータをレンダリングする

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+import {Comment} from "./class/comment";
+
+const COMMENTS: Comment[] = [
+  {name: 'aaa', message: 'haaaaaaaaaaaa!'},
+  {name: 'aaa', message: 'hiiiiiiii!'},
+  {name: 'bbb', message: 'huuuuuuuuuu!'},
+]
+
 @Component({
   selector: 'ac-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-chat';
+  comments = COMMENTS;
 }
