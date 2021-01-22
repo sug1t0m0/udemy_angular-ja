e24392198bd88a0e0b00e6a87a90a34897063ede
tomo_s <f15c017d@gmail.com>
2021-01-23 07:13:43 +0900

    Headerコンポーネントを作成して, coreモジュールに含めた

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -6,17 +6,21 @@ import {AngularFireAuthModule} from "@angular/fire/auth";
 import {AngularFireDatabaseModule} from "@angular/fire/database";
 
 import {environment} from "../../environments/environment";
+import { HeaderComponent } from './components/header/header.component';
 
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent],
   imports: [
     CommonModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     AngularFireDatabaseModule
+  ],
+  exports:[
+    HeaderComponent
   ]
 })
 export class CoreModule { }
