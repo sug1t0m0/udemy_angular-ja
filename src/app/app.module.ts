b349fe6ff99e70aee0e01c695c2ebc85a2ff826e
tomo_s <f15c017d@gmail.com>
2021-01-23 06:42:18 +0900

    Chatコンポーネントを作成しappコンポーネントの中身を移植

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import {AngularFireModule} from "@angular/fire";
 import {environment} from "../environments/environment";
 import {AngularFireAuthModule} from "@angular/fire/auth";
 import {AngularFireDatabaseModule} from "@angular/fire/database";
+import { ChatComponent } from './chat/chat.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CommentDatePipe
+    CommentDatePipe,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
