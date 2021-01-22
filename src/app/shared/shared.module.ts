c0aa0bf17a2619710e5e572886be7feead01c35b
tomo_s <f15c017d@gmail.com>
2021-01-23 07:04:29 +0900

    パスが違うので修正

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FormsModule} from "@angular/forms";
-import { CommentDatePipe } from './pipes/comment-date.pipe';
+import { CommentDatePipe } from '../pipes/comment-date.pipe';
 
 
 
