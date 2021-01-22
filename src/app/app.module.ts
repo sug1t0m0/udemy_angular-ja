de189ec00dd6a903c310a959aa6f7fef77a2edba
tomo_s <f15c017d@gmail.com>
2021-01-23 06:58:42 +0900

    モジュール分割を行った

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from "@angular/forms";
+
 
 import { AppComponent } from './app.component';
-import { CommentDatePipe } from './pipes/comment-date.pipe';
-import {AngularFireModule} from "@angular/fire";
-import {environment} from "../environments/environment";
-import {AngularFireAuthModule} from "@angular/fire/auth";
-import {AngularFireDatabaseModule} from "@angular/fire/database";
+
 import { ChatComponent } from './chat/chat.component';
+import {SharedModule} from "./shared/shared.module";
+import {CoreModule} from "./core/core.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    CommentDatePipe,
     ChatComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFireAuthModule,
-    AngularFireDatabaseModule
+    SharedModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
