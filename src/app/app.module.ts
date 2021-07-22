5d670fb543a2524c7f11c3117eae543ba074c1ed
sug1t0m0 <f15c017d@gmail.com>
2021-07-23 07:25:19 +0900

    set firebase project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CommentDatePipe } from './pipes/comment-date.pipe';
+import {AngularFireModule} from "@angular/fire";
+import {environment} from "../environments/environment";
+import {AngularFireAuthModule} from "@angular/fire/auth";
+import {AngularFireDatabaseModule} from "@angular/fire/database";
 
 
 @NgModule({
@@ -13,7 +17,10 @@ import { CommentDatePipe } from './pipes/comment-date.pipe';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
