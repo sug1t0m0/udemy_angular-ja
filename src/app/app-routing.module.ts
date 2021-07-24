ef107d90e7fa83fde04d63479561d9474aede28c
sug1t0m0 <f15c017d@gmail.com>
2021-07-24 18:52:27 +0900

    add routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from "@angular/router";
+import {ChatComponent} from "./chat/chat.component";
+import {NotFoundComponent} from "./core/components/not-found/not-found.component";
+
+const routes: Routes = [
+  {path: '', component: ChatComponent},
+  {path: '**', component: NotFoundComponent}
+]
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
